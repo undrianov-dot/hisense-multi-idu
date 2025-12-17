0f5891cc3ca48faa988cc9c119375ec2a69b3fd5
undrianov-dot <undrianov@gmail.com>
2025-12-17 12:43:54 +0300

    Update const.py

--- custom_components/hisense-multi-idu/const.py
+++ custom_components/hisense-multi-idu/const.py
@@ -1,18 +1,1 @@
 DOMAIN = "hisense_multi_idu"
-
-# Эти значения должны соответствовать твоему HiDom.
-# Если у тебя другие коды — просто поменяешь цифры.
-MODE_MAP = {
-    "cool": 2,
-    "dry": 4,
-    "fan_only": 8,
-    "heat": 16,
-}
-MODE_REVERSE_MAP = {v: k for k, v in MODE_MAP.items()}
-
-FAN_MAP = {
-    "low": 1,
-    "medium": 2,
-    "high": 3,
-}
-FAN_REVERSE_MAP = {v: k for k, v in FAN_MAP.items()}
